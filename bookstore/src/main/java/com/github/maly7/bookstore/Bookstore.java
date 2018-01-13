0a752ab4fd411d2dd4619f858bb1f42f682f3778
Paul Aly <paul.aly@outlook.com>
2018-01-13 09:19:08 -0500

    add bookstore application

---
+++ bookstore/src/main/java/com/github/maly7/bookstore/Bookstore.java
@@ -0,0 +1,11 @@
+package com.github.maly7.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bookstore {
+    public static void main(String[] args) {
+        SpringApplication.run(Bookstore.class, args);
+    }
+}
