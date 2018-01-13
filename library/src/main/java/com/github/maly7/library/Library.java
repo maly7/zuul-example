f674c488c48feebe9538366df5898f53669ba0c9
Paul Aly <paul.aly@outlook.com>
2018-01-13 09:21:21 -0500

    add library application

---
+++ library/src/main/java/com/github/maly7/library/Library.java
@@ -0,0 +1,11 @@
+package com.github.maly7.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Library {
+    public static void main(String[] args) {
+        SpringApplication.run(Library.class, args);
+    }
+}
